bfa5a3644f40dcdc8254ac753a18e328f43a358d
yoshi <yoshi.8867.0311@gmail.com>
2025-12-04 15:32:29 +0900

    카드 컴포저블 예제: 콜백 함수(클릭), Box로 겹치기, mutableStateOf 등

--- JetpackComposeStudy/app/src/main/java/com/example/jetpackcomposestudy/MainActivity.kt
+++ JetpackComposeStudy/app/src/main/java/com/example/jetpackcomposestudy/MainActivity.kt
@@ -4,34 +4,100 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.jetpackcomposestudy.ui.theme.JetpackComposeStudyTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContent {
-            MyComposable()
+            var isFavorite by rememberSaveable {
+                mutableStateOf(false)
+            }
+            StudyCard(
+                modifier = Modifier
+                    .fillMaxWidth(0.5f)
+                    .padding(10.dp),
+                isFavorite = isFavorite,
+            ) { favorite ->
+                isFavorite = favorite
+            }
         }
     }
 }
 
 @Composable
-fun MyComposable() {
+fun StudyCard(
+    modifier: Modifier = Modifier,
+    isFavorite: Boolean,
+    onTabFavorite: (Boolean) -> Unit,
+) {
+    Card(
+        modifier = modifier,
+        shape = RoundedCornerShape(10.dp),
+        elevation = CardDefaults.cardElevation(4.dp),
+    ) {
+        Box(
+            modifier = Modifier
+                .height(300.dp),
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.img_card_sample),
+                contentDescription = "card image",
+                contentScale = ContentScale.Crop,
+            )
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.TopEnd,
+            ) {
+                IconButton(onClick = {
+                    onTabFavorite(!isFavorite)
+                }) {
+                    Icon(
+                        imageVector = if (isFavorite) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
+                        contentDescription = "favorite",
+                        tint = Color.White,
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun StudyLazyColumnComposable() {
     LazyColumn(
         modifier = Modifier
             .background(color = Color.Green)
@@ -47,6 +113,6 @@ fun MyComposable() {
 
 @Preview(showBackground = true)
 @Composable
-fun MyPreview() {
-    MyComposable()
+fun StudyLazyColumnPreview() {
+
 }
