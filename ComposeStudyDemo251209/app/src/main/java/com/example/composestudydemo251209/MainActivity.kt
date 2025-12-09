5acbf16bd796d639b34905540a90c1f27c991541
yoshi <yoshi.8867.0311@gmail.com>
2025-12-09 16:39:06 +0900

    슬롯 추가 실습

--- ComposeStudyDemo251209/app/src/main/java/com/example/composestudydemo251209/MainActivity.kt
+++ ComposeStudyDemo251209/app/src/main/java/com/example/composestudydemo251209/MainActivity.kt
@@ -1,40 +1,143 @@
 package com.example.composestudydemo251209
 
-import android.R.attr.name
-import android.R.attr.onClick
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composestudydemo251209.ui.theme.ComposeStudyDemo251209Theme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ComposeStudyDemo251209Theme {
-
-            }
         }
     }
 }
 
 @Composable
-fun SlotDemo(middleContent: @Composable () -> Unit) {
-    Column {
-        Text("111")
-        middleContent()
-        Text("333")
+fun MainScreen() {
+    var linearSelected by remember { mutableStateOf(true) }
+    var imageSelected by remember { mutableStateOf(true) }
+
+    val onLinearClick = { value : Boolean ->
+        linearSelected = value
     }
+
+    val onTitleClick = { value : Boolean ->
+        imageSelected = value
+    }
+
+    ScreenContent(
+        linearSelected = linearSelected,
+        imageSelected = imageSelected,
+        onTitleClick = onTitleClick,
+        onLinearClick = onLinearClick,
+        titleContent = {
+            if (imageSelected) {
+                TitleImage(drawing = R.drawable.outline_cloud_download_24)
+            } else {
+                Text("Downloading",
+                    style = MaterialTheme.typography.headlineMedium,
+                    modifier = Modifier.padding(30.dp),
+                    )
+            }
+        },
+        progressContent = {
+            if (linearSelected) {
+                LinearProgressIndicator(Modifier.height(40.dp))
+            } else {
+                CircularProgressIndicator(
+                    modifier = Modifier.size(200.dp),
+                    strokeWidth = 18.dp
+                    )
+            }
+        },
+    )
+}
+
+@Composable
+fun ScreenContent(
+    linearSelected: Boolean,
+    imageSelected: Boolean,
+    onTitleClick: (Boolean) -> Unit,
+    onLinearClick: (Boolean) -> Unit,
+    titleContent: @Composable () -> Unit,
+    progressContent: @Composable () -> Unit,
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween,
+    ) {
+        titleContent()
+        progressContent()
+        Checkboxes(
+            imageSelected = imageSelected,
+            linearSelected = linearSelected,
+            onTitleClick = onTitleClick,
+            onLinearClick = onLinearClick,
+        )
+    }
+}
+
+@Composable
+fun Checkboxes(
+    imageSelected: Boolean,
+    linearSelected: Boolean,
+    onTitleClick: (Boolean) -> Unit,
+    onLinearClick: (Boolean) -> Unit,
+) {
+    Row(
+        Modifier.padding(20.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Checkbox(
+            checked = imageSelected,
+            onCheckedChange = onTitleClick,
+        )
+        Text("Image Title")
+        Spacer(modifier = Modifier.width(20.dp))
+        Checkbox(
+            checked = linearSelected,
+            onCheckedChange = onLinearClick,
+        )
+        Text("Linear Progress")
+    }
+}
+
+@Composable
+fun TitleImage(drawing: Int) {
+    Image(
+        painter = painterResource(drawing),
+        contentDescription = "title inage",
+    )
 }
 
 @Preview(showBackground = true)
@@ -42,14 +145,6 @@ fun SlotDemo(middleContent: @Composable () -> Unit) {
 fun Preview() {
     val txt = remember { mutableStateOf("GOOD") }
     ComposeStudyDemo251209Theme {
-        SlotDemo(
-            middleContent =  { // <- 이렇게 람다가 들어가야 함
-                Button(
-                    onClick = { txt.value = "HELLO" }
-                ) {
-                    Text(txt.value)
-                }
-            }
-        )
+        MainScreen()
     }
 }
