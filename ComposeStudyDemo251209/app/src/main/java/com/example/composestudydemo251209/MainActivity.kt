7edde0014394bbc91bf7463ca317bf92f7b77d5a
yoshi <yoshi.8867.0311@gmail.com>
2025-12-09 18:42:40 +0900

    modifier 연습

--- ComposeStudyDemo251209/app/src/main/java/com/example/composestudydemo251209/MainActivity.kt
+++ ComposeStudyDemo251209/app/src/main/java/com/example/composestudydemo251209/MainActivity.kt
@@ -5,6 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,6 +17,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.LinearProgressIndicator
@@ -26,9 +30,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composestudydemo251209.ui.theme.ComposeStudyDemo251209Theme
 
 
@@ -140,11 +148,51 @@ fun TitleImage(drawing: Int) {
     )
 }
 
-@Preview(showBackground = true)
+@Composable
+fun ModiStudyScreen() {
+    val modifier = Modifier
+        .border(width = 2.dp, color = Color.Black)
+        .padding(all = 10.dp)
+    Text(
+        text = "HELLO WORLD",
+        modifier = modifier,
+        fontSize = 40.sp,
+        fontWeight = FontWeight.Bold,
+    )
+}
+
+@Composable
+fun CustomImage(image: Int, modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(image),
+        contentDescription = "yoshi!",
+        modifier = modifier,
+    )
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true,
+)
 @Composable
 fun Preview() {
     val txt = remember { mutableStateOf("GOOD") }
     ComposeStudyDemo251209Theme {
-        MainScreen()
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+        ) {
+            ModiStudyScreen()
+            Spacer(modifier = Modifier.height(10.dp))
+            CustomImage(
+                image = R.drawable.yoshi,
+                Modifier
+                    .border(width = 2.dp, color = Color.Black)
+                    .padding(all = 10.dp)
+                    .clip(shape = RoundedCornerShape(60.dp))
+
+            )
+        }
     }
 }
